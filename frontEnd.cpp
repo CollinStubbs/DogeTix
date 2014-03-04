e05cf0c3dc70fc820ce73948f035ffc18bddb5bd
mattmili <matthewmilitante@gmail.com>
2014-03-04 12:48:55 -0500

    Initial init

---
+++ frontEnd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+	Reads in 2 files:
+	1) file containing information regarding current user accounts
+	2) file of tickets available for purchase
+*/
+int main(){
+
+
+
+	return 0;
+} 
