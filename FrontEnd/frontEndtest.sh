fed47bfd13deba9eb928038fe4a94455db9a667c
mattmili <matthewmilitante@gmail.com>
2014-03-14 14:45:28 -0400

    added login_unpriv test script

--- FrontEnd/frontEndtest.sh
+++ FrontEnd/frontEndtest.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # make all scripts executable
-echo "Running all front end tests"
-
+chmod +x ./login_testscripts/'*'.sh 
+echo "RUNNING ALL FRONT END TESTS"
 echo $'\n'
-echo "Running login_first_transaction"
+echo "RUNNING login_first_transaction TO GENERATE A SUCCESSFUL LOGIN"
 ./login_testscripts/login_first_transaction.sh
 echo $'\n'
-echo "Running login_first_transaction to generate an error"
+echo "RUNNING login_first_transaction TO GENERATE AN ERROR"
 ./login_testscripts/login_first_transaction_error.sh
 echo $'\n'
-echo "Running login_sublogin to generate an error"
+echo "RUNNING login_sublogin TO GENERATE AND ERROR IN TRYING TO LOGIN AGAIN"
 ./login_testscripts/login_sublogin.sh
+echo $'\n'
+echo "RUNNING login_unpriv TO PERFORM PRIVILAGED TRANSACTION ON UNPRIVILAGED USERNAME"
+./login_testscripts/login_unpriv.sh
