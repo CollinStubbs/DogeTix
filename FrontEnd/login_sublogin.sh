24c2bd25f1af727ef51673a2ed7b05bc6f35f8ee
Matthew Militante <matthewmilitante@gmail.com>
2014-03-14 14:33:49 -0400

    Delete login_sublogin.sh

--- FrontEnd/login_sublogin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# run the program using the make file
-make
-# test the file with username ADMIN COLLINSTUBBSSSS
-command1='login COLLINSTUBBSSSS login'
-echo $command1 | ./DTFE tickets.txt user_info.txt
-
-make clean
