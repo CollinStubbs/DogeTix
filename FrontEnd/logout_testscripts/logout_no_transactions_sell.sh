e229cd08b6229e1643803f3a210b4469382285f5
CollinStubbs <collin.stubbs@uoit.net>
2014-03-14 19:38:26 -0400

    multiple logout tests added

---
+++ FrontEnd/logout_testscripts/logout_no_transactions_sell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run the program using the make file
+make
+# test the file with logout as first input to generate an error
+command1='sell'
+echo $command1 | ./DTFE tickets.txt user_info.txt
+
+make clean
