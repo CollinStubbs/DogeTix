921bb9e843d94286f3a4458755d6904bc2be5d6b
mattmili <matthewmilitante@gmail.com>
2014-03-14 14:32:26 -0400

    placed all test scripts for login inside login_testscripts

---
+++ FrontEnd/login_testscripts/login_first_transaction_error.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run the program using the make file
+make
+# test the file with create as a first input to generate an error
+command1='create'
+echo $command1 | ./DTFE tickets.txt user_info.txt
+
+make clean
