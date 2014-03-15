6a9f4d17662d47923b64058442fd7def4134cbc0
mattmili <matthewmilitante@gmail.com>
2014-03-15 15:51:21 -0400

    merged logout_testscripts with frontend.sh

--- FrontEnd/frontEndtest.sh
+++ FrontEnd/frontEndtest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # make all scripts executable
-chmod +x ./login_testscripts/'*'.sh 
-echo "RUNNING ALL FRONT END TESTS"
 echo $'\n'
+echo "**********RUNNING ALL FRONT END TESTS**********"
+echo $'\n'
+# test login test cases
 echo "RUNNING login_first_transaction TO GENERATE A SUCCESSFUL LOGIN"
 ./login_testscripts/login_first_transaction.sh
 echo $'\n'
@@ -15,3 +16,10 @@ echo "RUNNING login_sublogin TO GENERATE AND ERROR IN TRYING TO LOGIN AGAIN"
 echo $'\n'
 echo "RUNNING login_unpriv TO PERFORM PRIVILAGED TRANSACTION ON UNPRIVILAGED USERNAME"
 ./login_testscripts/login_unpriv.sh
+echo $'\n'
+# Test logout test cases
+echo "RUNNING login_valid TO GENERATE ERROR WHEN LOGOUT IS FIRST COMMAND"
+./logout_testscripts/logout_valid.sh
+echo $'\n'
+echo "RUNNING login_no_transactions TO TEST THAT NO TRANSACTIONS ARE ACCEPTED AFTER logout"
+./logout_testscripts/logout_no_transactions.sh
