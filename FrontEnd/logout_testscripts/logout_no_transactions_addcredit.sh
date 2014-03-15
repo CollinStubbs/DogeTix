ec993641b3d669390635a02b6809abf11ab3ec87
Matthew Militante <matthewmilitante@gmail.com>
2014-03-15 15:53:58 -0400

    Delete logout_no_transactions_addcredit.sh

--- FrontEnd/logout_testscripts/logout_no_transactions_addcredit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# run the program using the make file
-make
-# test the file with addcredit as first input to generate an error
-command1='addcredit'
-echo $command1 | ./DTFE tickets.txt user_info.txt
-
-make clean
