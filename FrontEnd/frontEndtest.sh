41b269c72f27cb0e306898d513627ab21186b961
mattmili <matthewmilitante@gmail.com>
2014-03-16 12:21:47 -0400

    moved test scripts to input folder, deleted old ones, added test script to test privilage transaction under privilaged user

--- FrontEnd/frontEndtest.sh
+++ FrontEnd/frontEndtest.sh
@@ -6,20 +6,30 @@ echo "**********RUNNING ALL FRONT END TESTS**********"
 echo $'\n'
 # test login test cases
 echo "RUNNING login_first_transaction TO GENERATE A SUCCESSFUL LOGIN"
-./login_testscripts/login_first_transaction.sh
+./inputs/login_testscripts/login_first_transaction.sh
+
 echo $'\n'
 echo "RUNNING login_first_transaction TO GENERATE AN ERROR"
-./login_testscripts/login_first_transaction_error.sh
+./inputs/login_testscripts/login_first_transaction_error.sh
+
 echo $'\n'
 echo "RUNNING login_sublogin TO GENERATE AND ERROR IN TRYING TO LOGIN AGAIN"
-./login_testscripts/login_sublogin.sh
+./inputs/login_testscripts/login_sublogin.sh
+
 echo $'\n'
 echo "RUNNING login_unpriv TO PERFORM PRIVILAGED TRANSACTION ON UNPRIVILAGED USERNAME"
-./login_testscripts/login_unpriv.sh
+./inputs/login_testscripts/login_unpriv.sh
+
 echo $'\n'
+echo "RUNNING login_priv TO PERFORM PRIVILAGED TRANSACTION ON PRIVILAGED USERNAME"
+./inputs/login_testscripts/login_priv.sh
+
+
 # Test logout test cases
+echo $'\n'
 echo "RUNNING login_valid TO GENERATE ERROR WHEN LOGOUT IS FIRST COMMAND"
-./logout_testscripts/logout_valid.sh
+./inputs/logout_testscripts/logout_valid.sh
+
 echo $'\n'
 echo "RUNNING login_no_transactions TO TEST THAT NO TRANSACTIONS ARE ACCEPTED AFTER logout"
-./logout_testscripts/logout_no_transactions.sh
+./inputs/logout_testscripts/logout_no_transactions.sh
