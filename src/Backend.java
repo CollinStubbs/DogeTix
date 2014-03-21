25a032acdc29cb69ca278476bf60976b60d52e91
mattmili <matthewmilitante@gmail.com>
2014-03-21 15:28:33 -0400

    started backend

--- src/Backend.java
+++ src/Backend.java
@@ -13,11 +13,41 @@
  *
  */
 import java.util.*;
+import java.io.*;
+
 
 public class Backend{
-	public static void main(String[] args){
 
-		
-		
-	}
+    public String readFile(String fileName){
+        String returnValue = "";
+        FileReader file = null;
+        String line = "";
+        try {
+            file = new FileReader(fileName);
+            BufferedReader reader = new BufferedReader(file);
+            while ((line = reader.readLine()) != null) {
+                returnValue += line + "\n";
+            }
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found");
+        }catch (IOException e){
+            throw new RuntimeException("IO Error occured");
+        }finally{
+            if (file != null) {
+                try{
+                    file.close();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return returnValue;
+    }
+
+    public static void main(String[] args){
+
+        String inputDTFfile = "DailyTransactionFile.txt";
+
+
+    }
 }
