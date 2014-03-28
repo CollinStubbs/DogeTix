d574536fba3e1f3b1a1bdd1651a2ba9712f53d39
CollinStubbs <collin.stubbs@uoit.net>
2014-03-28 14:59:40 -0400

    finished error log manager

--- src/BackEnd/ErrorLogManager.java
+++ src/BackEnd/ErrorLogManager.java
@@ -1,15 +1,22 @@
 package BackEnd;
+/*************************************
+   This is a class designed to handle
+  errors within the code. If an error is
+  found an instance of this class will be
+  created to deal with it.
+**************************************/
 
 public class ErrorLogManager{
    static String type;
    static String desc;
    static String transaction;
-   
+   //constructor for fatal errors
    public ErrorLogManager(String type, String desc){
       this.type = type;
       this.desc = desc;
       writeToLog();
    }
+   //constructor for constraint errors
    public ErrorLogManager(String type, String desc, String transaction){
       this.type = type;
       this.desc = desc;
